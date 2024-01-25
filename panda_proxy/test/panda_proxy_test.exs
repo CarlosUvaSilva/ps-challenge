18f66ef4ca9648d4027ee717b5e3dcff4184d90a
Carlos Silva <carlos.uva.silva@gmail.com>
2024-01-25 16:00:35 +0000

    test-proxy: create some basic tests for the proxy main features

--- panda_proxy/test/panda_proxy_test.exs
+++ panda_proxy/test/panda_proxy_test.exs
@@ -1,8 +1,28 @@
-defmodule PandaTest do
+defmodule PandaProxyTest do
   use ExUnit.Case
-  doctest Panda
+  alias Panda
 
-  test "greets the world" do
-    assert Panda.hello() == :world
+  describe "render_matches/1" do
+    test "renders matches successfully" do
+      assert {:ok, _} = Panda.render_matches()
+    end
+  end
+
+  describe "upcoming_matches/1" do
+    test "fetches matches successfully" do
+      matches = Panda.upcoming_matches()
+      assert is_list(matches)
+      Enum.each(matches, &assert is_map(&1))
+    end
+
+    test "fetches matches successfully with filter" do
+      matches = Panda.upcoming_matches("fnatic")
+      assert is_list(matches)
+      Enum.each(matches, fn match ->
+        assert is_map(match)
+        assert Map.has_key?(match, "name")
+        assert String.contains?(match["name"], "FNC")
+      end)
+    end
   end
 end
