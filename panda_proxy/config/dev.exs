00ed5c85fbd2f8f0f86b580fbca56c20ec58e5af
Carlos Silva <carlos.uva.silva@gmail.com>
2024-01-26 15:58:31 +0000

    fix-proxy: create a dev envronment config to allow for iex -S mix usage

---
+++ panda_proxy/config/dev.exs
@@ -0,0 +1,11 @@
+import Config
+
+config :panda_proxy,
+  ecto_repos: [Panda.Repo]
+
+config :panda_proxy, Panda.Repo,
+  database: "pandascore-challenge",
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  port: 5432
